dc6776135e3e1801b0569564b1f46f807e310808
Gustavo Pinto <gustavo.pinto@blip.pt>
2025-07-08 14:00:58 +0100

    test

--- data/source/memoryobfuscation/none.c
+++ data/source/memoryobfuscation/none.c
@@ -1,5 +1,23 @@
 void memoryobfuscation(){
-  FILE* f = fopen('C:\\something.txt', 'wb');
-  fwrite("HELLO!123\n", 1, f);
-  fclose(f);
+  HANDLE hFile;
+  DWORD bytesWritten;
+  const char* data = "{{PROCESS_SPAWN}}";
+  DWORD dataLen = (DWORD)lstrlenA(data);  // Use ANSI version
+
+  hFile = CreateFileA(
+        "C:\\output.txt",             // File name
+        GENERIC_WRITE,            // Write access
+        0,                        // No sharing
+        NULL,                     // Default security
+        CREATE_ALWAYS,            // Overwrite if exists
+        FILE_ATTRIBUTE_NORMAL,    // Normal file
+        NULL                      // No template
+  );
+
+  if (hFile == INVALID_HANDLE_VALUE)
+        return 1;
+
+  WriteFile(hFile, data, dataLen, &bytesWritten, NULL);
+  CloseHandle(hFile);
+
 }
--- data/source/memoryobfuscation/spawn.c
+++ data/source/memoryobfuscation/spawn.c
@@ -1,5 +1,24 @@
 void memoryobfuscation(){
-  FILE* f = fopen('C:\\something.txt', 'wb');
-  fwrite(&{{PROCESS_SPAWN}}, 1, f);
-  fclose(f);
+  HANDLE hFile;
+  DWORD bytesWritten;
+  const char* data = "Hello from WinAPI!";
+  DWORD dataLen = (DWORD)lstrlenA(data);  // Use ANSI version
+
+  hFile = CreateFileA(
+        "C:\\output.txt",             // File name
+        GENERIC_WRITE,            // Write access
+        0,                        // No sharing
+        NULL,                     // Default security
+        CREATE_ALWAYS,            // Overwrite if exists
+        FILE_ATTRIBUTE_NORMAL,    // Normal file
+        NULL                      // No template
+  );
+
+  if (hFile == INVALID_HANDLE_VALUE)
+        return 1;
+
+  WriteFile(hFile, data, dataLen, &bytesWritten, NULL);
+  CloseHandle(hFile);
+
+  //TODO fwrite(&{{PROCESS_SPAWN}}, 1, f);
 }
